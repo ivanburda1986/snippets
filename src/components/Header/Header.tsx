649ab3b5c1c9d8ca1c4fc523931ea3688e69dc3e
ivanburda1986 <burda.ivan@gmail.com>
2021-11-15 22:44:36 +0100

    header options

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
+import sharedStyles from "../sharedStyles/sharedStyles.module.css";
 
 export function Header() {
-  return <div className={classes.header}>My favourite snippets</div>;
+  return (
+    <div className={classes.header}>
+      <div className={classes.headerTitle}>Snippets</div>
+      <div className={sharedStyles.container}>
+        <div className={classes.headerOptions}>Label label</div>
+      </div>
+    </div>
+  );
 }

14a68b920df0e90095630c9b7e002bd6899c01d6
ivanburda1986 <burda.ivan@gmail.com>
2021-11-23 23:21:15 +0100

    action

--- src/components/AddSnippetForm/AddSnippetForm.tsx
+++ src/components/AddSnippetForm/AddSnippetForm.tsx
@@ -18,8 +18,8 @@ export const AddSnippetForm = React.memo(() => {
     setContent("");
   };
 
-  const toggleAssignedLabel = (labelName: labelData) => {
-    setAssignedLabels((prevAssignedLabels) => [...prevAssignedLabels].concat(labelName));
+  const toggleAssignedLabel = (assignedLabelData: labelData) => {
+    setAssignedLabels((prevAssignedLabels) => [...prevAssignedLabels].concat(assignedLabelData));
   };
 
   const submitHandler = (event: React.ChangeEvent<HTMLFormElement>) => {
@@ -44,7 +44,11 @@ export const AddSnippetForm = React.memo(() => {
         <input type="text" id="SnippetInputDescription" name="SnippetInputDescription" value={description} onChange={(event) => setDescription(event.target.value)} />
         <label htmlFor="SnippetInputContent">Snippet</label>
         <textarea id="SnippetInputDescription" name="SnippetInputDescription" rows={4} cols={50} value={content} onChange={(event) => setContent(event.target.value)} />
-        <div className={styles.labelSelection}></div>
+        <div className={styles.labelSelection}>
+          {labels.map((item) => (
+            <Label key={item.name} name={item.name} language={item.language} bgColor={item.bgColor} toggleAction={toggleAssignedLabel} />
+          ))}
+        </div>
         <div>
           <button type="submit">Submit</button>
           <button type="button" onClick={mycontext.toggleAddSnippetFormDisplay}>
